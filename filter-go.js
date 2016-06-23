e98d6a3a2498cb7b353e62f264a9f0c642ad4776
Ridderbusch <frank.ridderbusch@ts.fujitsu.com>
2016-06-23 15:39:31 +0200

    The usage of Typescript appears to be working.

--- filter-go.js
+++ filter-go.js
@@ -1,37 +1,35 @@
 #!/usr/bin/env node
-
 "use strict";
-
-// Pandoc filter to convert all text to uppercase
-
 var pandoc = require("pandoc-filter");
-
 var Str = pandoc.Str;
+var startGoTemplate = "{{<";
+var endGoTemplate = ">}}";
 var ignore = false;
-var startGoTemplate = "{{<"
-var endGoTemplate = ">}}"
-
 function action(type, value, format, meta) {
-  if (type === 'Str') {
-    if (value === startGoTemplate) {
-      ignore = true;
-      return [];
+    if (type === "Str") {
+        if (value === startGoTemplate) {
+            ignore = true;
+            return [];
+        }
+        if (value.indexOf(startGoTemplate) > 0) {
+            value = value.replace(startGoTemplate, "");
+            var s = value.indexOf("[");
+            s++;
+            var e = value.indexOf("]");
+            ignore = true;
+            return Str(value.substr(s, e - s));
+        }
+        if (value === endGoTemplate) {
+            ignore = false;
+            return [];
+        }
+        if (value.indexOf(endGoTemplate + ")") >= 0) {
+            value = value.replace(endGoTemplate + ")", "");
+            ignore = false;
+            return Str(value);
+        }
     }
-    if (value.indexOf(startGoTemplate) > 0) {
-      value = value.replace(startGoTemplate, "")
-      return Str(value)
-    }
-    if (value === endGoTemplate) {
-      ignore = false;
-      return [];
-    }
-    if (value.indexOf(endGoTemplate) == 0) {
-      value = value.replace(endGoTemplate, "")
-      return Str(value)
-    }
-  }
-  if (ignore == true)
-    return [];
+    if (ignore === true)
+        return [];
 }
-
 pandoc.stdio(action);
--- filter-go.ts
+++ filter-go.ts
@@ -1,26 +1,46 @@
 #!/usr/bin/env node
 
+///// <reference path="typings/index.d.ts" />
+
+declare var require: any;
+
 "use strict";
 
-// Pandoc filter to convert all text to uppercase
+const pandoc = require("pandoc-filter");
 
-import * as pandoc from "pandoc-filter";
+const Str = pandoc.Str;
+const startGoTemplate = "{{<";
+const endGoTemplate = ">}}";
 
-var Str: string = pandoc.Str;
 let ignore: boolean = false;
 
 function action(type: string, value: string, format: string, meta: string) {
-    if (type === 'Str') {
-        if (value === '{{<') {
-            ignore = true;
-            return [];
-        }
-        if (value === '>}}') {
-            ignore = false;
-            return [];
-        }
+  if (type === "Str") {
+    if (value === startGoTemplate) {
+      ignore = true;
+      return [];
     }
-    if (ignore == true) return [];
+    if (value.indexOf(startGoTemplate) > 0) {
+      value = value.replace(startGoTemplate, "");
+      let s = value.indexOf("[");
+      s++;
+      let e = value.indexOf("]");
+      ignore = true;
+      return Str(value.substr(s, e - s));
+    }
+    if (value === endGoTemplate) {
+      ignore = false;
+      return [];
+    }
+    if (value.indexOf(endGoTemplate + ")") >= 0) {
+      value = value.replace(endGoTemplate + ")", "");
+      ignore = false;
+      return Str(value);
+    }
+  }
+  if (ignore === true) return [];
 }
 
 pandoc.stdio(action);
+
+
